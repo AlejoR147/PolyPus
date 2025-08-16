3f73795f850084b7a144b373c282aec8f9e24ce2
AlejoR147 <ronan.roman10@gmail.com>
2025-08-15 20:31:28 -0500

    Inicio

---
+++ app/src/main/java/com/example/smart/MainActivity.kt
@@ -0,0 +1,67 @@
+package com.example.smart
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.smart.R.layout.inicio_sesion
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        setContentView(inicio_sesion)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+    }
+    fun mostrarRegistro(view: View) {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        setContentView(R.layout.registro)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main_registro)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+    fun mostrarInicio(view: View) {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        setContentView(R.layout.inicio_sesion)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+    fun mostrarMenu(view: View) {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        setContentView(R.layout.menu)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+    fun mostrarAyuda(view: View) {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        setContentView(R.layout.ayuda)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
